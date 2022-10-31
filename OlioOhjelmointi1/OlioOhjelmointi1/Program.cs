fef1b3f208e17616cc255f0e54865bcc3ccc7c82
Miro Luikku <91785715+purkajaXD@users.noreply.github.com>
2022-10-31 09:05:39 +0200

    Add files via upload

---
+++ OlioOhjelmointi1/OlioOhjelmointi1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OlioOhjelmointi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bird b = new Bird();
+            b.name = "Tweety";
+
+            Bird b2 = new Bird();
+            b2.name = "Woody";
+
+
+            Bird[] birds = new Bird[100];
+
+            for (int i = 0; i < birds.Length; i++)
+            {
+                birds[i] = new Bird();
+                birds[i].name = "Bird" + i;
+            }
+
+            for (int i = 0; i < birds.Length; i++)
+            {
+                birds[i].Chirp();
+            }
+
+     
+        }
+
+        public class Bird
+        {
+            public string name;
+
+
+            public void Chirp()
+            {
+                System.Console.WriteLine(name + " says chrip!");
+            }
+
+        }
+    }
+}
